623817987d6af4d544c5e2a5e4da3951d8321464
oleksandr <oleksandr.pavliuk1308@gmail.com>
2022-04-25 15:35:31 +0300

    eslint indent

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,33 @@
 showHello('greeting', 'TypeScript');
 
 function showHello(divName: string, name: string) {
-    const elt = document.getElementById(divName);
-    elt.innerText = `Hello from ${name}`;
+  const elt = document.getElementById(divName);
+  elt.innerText = `Hello from ${name}`;
 }
 
 enum Category { A = 'JavaScript', B = 'CSS', C = 'HTML' }
 
 type Category1 = {
-    A: 'JavaScript';
-    B: 'CSS';
-    C: 'HTML';
+  A: 'JavaScript';
+  B: 'CSS';
+  C: 'HTML';
 };
 
 
-function getAllBooks() {
-    const books = [
-        { id: 1, title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true },
-        { id: 2, title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false },
-        { id: 3, title: 'CSS Secrets', author: 'Lea Verou', available: true },
-        { id: 4, title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true }
-    ];
+type Book = {
+  id: number;
+  title: string;
+  author: string;
+  available: boolean;
+};
 
+function getAllBooks(): Book[] {
+  const books = [
+    { id: 1, title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true },
+    { id: 2, title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false },
+    { id: 3, title: 'CSS Secrets', author: 'Lea Verou', available: true },
+    { id: 4, title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true }
+  ];
+
+  return books;
 }
