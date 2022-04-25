d78b08d1c42360df6577cc8b8ae23cdb7f9fa06b
oleksandr <oleksandr.pavliuk1308@gmail.com>
2022-04-25 15:24:34 +0300

    First lessons

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,12 @@ function showHello(divName: string, name: string) {
     const elt = document.getElementById(divName);
     elt.innerText = `Hello from ${name}`;
 }
+
+enum Category { A = 'JavaScript', B = 'CSS', C = 'HTML' }
+
+type Category1 = {
+    A: 'JavaScript';
+    B: 'CSS';
+    C: 'HTML';
+};
+
